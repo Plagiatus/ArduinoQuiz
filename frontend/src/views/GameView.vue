aeaa536e479653e490dcecc6eb3cef3217a43549
Plagiatus <lukas.scheuerle@web.de>
2022-11-21 17:28:47 +0200

    style adjustments for different screens

--- frontend/src/views/GameView.vue
+++ frontend/src/views/GameView.vue
@@ -1,22 +1,26 @@
 <template>
     <div id="gameview">
-        <div id="player-wrapper" v-if="isFirstCome || isLockout">
-            <div class="player" v-for="player of gameData.players">
-                <div class="player-status" :class="{locked: player.locked, active: player.active}" :style="{width: dimensionsBasedOnPlayer, height: dimensionsBasedOnPlayer,}">
+        <div id="player-wrapper" v-if="isFirstCome || isLockout" :style="{fontSize: `clamp(1em, calc(8vw / ${gameData.players.length}), 4em)` }">
+            <div class="player" v-for="player of     gameData.players">
+                <div class="player-status" :class="{ locked: player.locked, active: player.active }"
+                    :style="{ width: dimensionsBasedOnPlayer, height: dimensionsBasedOnPlayer }">
                     <img class="player-status-locked" src="/close.svg" v-if="player.locked">
                 </div>
-                <div v-if="!displayOnly">
+                <div class="name-and-score-wrapper" v-if="!displayOnly">
                     <input type="text" v-model="player.name">
-                    <input type="number" v-model="player.points" v-if="!isNaN(player.points)">
+                    <input type="number" v-model="player.points" v-if="pointsVisible">
                 </div>
-                <div v-else>
-                    <span>{{player.name}}</span>
-                    <span>{{player.points}}</span>
+                <div class="name-and-score-wrapper" v-else>
+                    <span>{{ player.name }}</span>
+                    <span v-if="pointsVisible">{{ player.points }}</span>
                 </div>
             </div>
         </div>
-        <button @click="newGame" v-if="!displayOnly">New Game</button>
-        <KeyboardControls v-if="controlView || showKeyboardControls"/>
+        <div id="controls-wrapper" v-if="!displayOnly">
+            <ToggleButton :text="'Show Points'" :enabled="pointsVisible" @click="togglePointsVisibility" />
+            <button @click="newGame">New Game</button>
+        </div>
+        <!-- <KeyboardControls v-if="controlView || showKeyboardControls" /> -->
     </div>
 </template>
 
@@ -25,10 +29,11 @@ import { defineComponent } from 'vue';
 import * as Socket from "../composeables/socket";
 import { GameData, GameType } from '../types';
 import KeyboardControls from "../components/KeyboardControls.vue";
+import ToggleButton from "../components/ToggleButton.vue";
 
 
 export default defineComponent({
-    components: { KeyboardControls },
+    components: { KeyboardControls, ToggleButton },
     props: {
         displayOnly: Boolean,
         controlView: Boolean,
@@ -39,6 +44,7 @@ export default defineComponent({
             gameData: {} as GameData,
             showKeyboardControls: false,
             hasRecievedNewData: false,
+            pointsVisible: true,
         }
     },
     methods: {
@@ -52,8 +58,8 @@ export default defineComponent({
             clearTimeout(this.initTimeout);
             let playerAmount = NaN;
             let gameType = 0;
-            
-            playerAmount = 6;
+
+            playerAmount = 2;
             gameType = 1;
 
             while (isNaN(playerAmount) || playerAmount <= 1) {
@@ -70,7 +76,7 @@ export default defineComponent({
                 players: []
             }
             for (let i: number = 0; i < playerAmount; i++) {
-                this.gameData.players.push({ active: false, locked: false, points: NaN, name: "" });
+                this.gameData.players.push({ active: false, locked: false, points: 0, name: "" });
             }
             this.handleSocketRequest("gameData");
         },
@@ -91,14 +97,15 @@ export default defineComponent({
                     break;
             }
         },
-        keyControls(event: KeyboardEvent){
-            const {altKey, ctrlKey, shiftKey, key} = event;
-            console.log({altKey, ctrlKey, shiftKey, key});
+        keyControls(event: KeyboardEvent) {
+            const { altKey, ctrlKey, shiftKey, key } = event;
+            console.log({ altKey, ctrlKey, shiftKey, key });
             // event.preventDefault();
         },
-        handleHardwareCommands(data: any){
+        handleHardwareCommands(data: any) {
             console.log("hardware", data);
-        }
+        },
+        togglePointsVisibility() { this.pointsVisible = !this.pointsVisible },
     },
     computed: {
         isFirstCome(): boolean {
@@ -117,8 +124,8 @@ export default defineComponent({
     },
     watch: {
         gameData: {
-            handler(newData, oldData){
-                if(!this.hasRecievedNewData) Socket.socketSendMessage("newData", newData);
+            handler(newData, oldData) {
+                if (!this.hasRecievedNewData) Socket.socketSendMessage("newData", newData);
                 this.hasRecievedNewData = false;
             },
             deep: true,
@@ -129,8 +136,8 @@ export default defineComponent({
         Socket.socketListen("request", this.handleSocketRequest);
         Socket.socketListen("hardware", this.handleHardwareCommands);
         this.initGameData();
-        
-        if(!this.displayOnly) {
+
+        if (!this.displayOnly) {
             window.addEventListener("keydown", this.keyControls);
         }
     },
@@ -138,7 +145,6 @@ export default defineComponent({
 </script>
 
 <style>
-
 #gameview {
     padding: 2em;
 }
@@ -167,24 +173,41 @@ code {
     margin-bottom: 1em;
     min-height: 10vw;
     min-width: 10vw;
-    max-height: 30vw;
-    max-width: 30vw;
+    max-height: 50vh;
+    max-width: 50vh;
 }
+
 .player-status.active {
     background-color: aquamarine;
 }
+
 .player-status.locked {
     border-color: var(--red);
     background-color: var(--red-light);
 }
+
 .player-status-locked {
     width: 100%;
     height: 100%;
     filter: var(--red-filter)
 }
 
-input {
-    text-align: center;
+.name-and-score-wrapper {
+    width: 100%;
 }
 
+.name-and-score-wrapper>input,
+.name-and-score-wrapper>span {
+    text-align: center;
+    border: none;
+    display: block;
+    width: 100%;
+    margin-bottom: 0.2em;
+}
+
+input[type="number"] {
+    -webkit-appearance: textfield;
+    -moz-appearance: textfield;
+    appearance: textfield;
+}
 </style>
